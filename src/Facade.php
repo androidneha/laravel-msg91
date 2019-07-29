426db66459c6093bd8ce8ec2527ca408ff72acc0
Vaibhav Pandey <contact@vaibhavpandey.com>
2019-07-29 08:40:15 +0530

    Update facade phpdoc comments.

--- src/Facade.php
+++ src/Facade.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade as LaravelFacade;
  * @package Laravel\Msg91
  *
  * @method static bool otp(string $number, string $sender = null, string $message = null)
- * @method static string|false sms(string $number, string $message, string $sender = null, int $route = null, string $country = null)
+ * @method static string|false sms(string|array|null $number, string|array $message, string $sender = null, int $route = null, string $country = null)
  * @method static bool verify(string $number, string $otp)
  */
 class Facade extends LaravelFacade
